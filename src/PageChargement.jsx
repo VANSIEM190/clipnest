653c3d819f6e181f3240bc55b536e9a4c016f2c7
VANSIEM190 <vansiemgracia03@gmail.com>
2025-04-12 15:08:56 +0100

    upload

--- src/PageChargement.jsx
+++ src/PageChargement.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import  logoApp  from './assets/image.png';
 
 const PageChargement = () => {
@@ -8,11 +7,9 @@ const PageChargement = () => {
       <div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-100">
         <img src={logoApp} alt="logo-app" />
         <h1 className="text-3xl  font-bold mt-4">SnapLoop</h1>
-        <Link to="/connexion" >
         <button type="button" className="bg-gray-800 text-white p-3 rounded-md cursor-pointer  mt-4">
           Contunuer
           </button>
-        </Link>
       </div>
     </>
   );
