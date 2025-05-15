9dfde28ce5649428947e440c2e564631d2e59479
VANSIEM190 <vansiemgracia03@gmail.com>
2025-05-15 01:07:01 +0100

    mise à jour du fichier main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import RouterApp from './router/RouterApp'
 import { UserProvider } from './Context/UserContext'; 
 import { DarkModeProvider } from './Context/DarkModeContext'; 
-import {PresenceProvider} from './Context/presenceContext'
+import {PresenceProvider} from './Context/PresenceContext'
 
 
 createRoot(document.getElementById('root')).render(
