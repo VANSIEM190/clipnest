43861488be2a7c66aa0658eb01246710e2085f40
VANSIEM190 <vansiemgracia03@gmail.com>
2025-05-15 01:46:26 +0100

    update

--- Back-end/index.js
+++ Back-end/index.js
@@ -6,7 +6,11 @@ const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://clipnest-zeta.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}))
 app.use("/api/firebase", firebaseRoute)
 
 app.listen(port, () => console.log(` app listening on port ${port}!`))
