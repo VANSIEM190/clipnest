ee9ad053849c7ca99426f59e6d59387e45571940
VANSIEM190 <vansiemgracia03@gmail.com>
2025-07-22 23:01:45 +0100

    mise à jour

--- src/components/comments/index.js
+++ src/components/comments/index.js
@@ -1,3 +1,3 @@
 export { default as AfficheCommentaireUsers } from './AfficheCommentaireUsers.jsx';
 export { default as SectionDeCommentaire } from './SectionDeCommentaire.jsx';
-export { default as commentairesUsers } from './commentairesUsers.jsx';
+export { default as CommentsUsers } from './commentairesUsers.jsx'
