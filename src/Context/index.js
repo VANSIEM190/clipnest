790e8538a684a2f1ff349941657b47e36e8190fd
VANSIEM190 <vansiemgracia03@gmail.com>
2025-07-24 02:28:17 +0100

    mise à jour

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,3 +1,3 @@
-export {UserProvider , useUser } from './UserContext.jsx';
-export { NetworkStatusProvider, useNetworkStatus } from './NetworkStatusContext.jsx';
-export { DarkModeProvider , useDarkMode  } from './DarkModeContext.jsx'; 
+export {DarkModeProvider , useDarkMode} from './DarkModeContext.jsx'
+export {NetworkStatusProvider , useNetworkStatus} from './NetworkStatusContext.jsx'
+export {UserProvider , useUser} from './UserContext.jsx'
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import RouterApp from './router/RouterApp'
-// import { NetworkStatusProvider } from './context/NetworkStatusContext.jsx'
-// import { DarkModeProvider } from './context/DarkModeContext.jsx'
-// import { UserProvider } from './context/UserContext.jsx'
+import { NetworkStatusProvider } from './context/NetworkStatusContext.jsx'
+import { UserProvider } from './context/UserContext.jsx'
+import { DarkModeProvider } from './context/DarkModeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <RouterApp />
+      <NetworkStatusProvider>
+        <UserProvider>
+          <DarkModeProvider>
+            <RouterApp />
+          </DarkModeProvider>
+        </UserProvider>
+      </NetworkStatusProvider>
     </BrowserRouter>
   </StrictMode>
 )
