ff517275e53d0879c63b59dc428e9d3abc6529cb
VANSIEM190 <vansiemgracia03@gmail.com>
2025-07-24 03:21:31 +0100

    mise à jour

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import RouterApp from './router/RouterApp'
-import { NetworkStatusProvider } from './context/NetworkStatusContext.jsx'
-import { UserProvider } from './context/UserContext.jsx'
-import { DarkModeProvider } from './context/DarkModeContext.jsx'
+// import { NetworkStatusProvider } from './context/NetworkStatusContext.jsx'
+// import { UserProvider } from './context/UserContext.jsx'
+// import { DarkModeProvider } from './context/DarkModeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <NetworkStatusProvider>
-        <UserProvider>
-          <DarkModeProvider>
-            <RouterApp />
-          </DarkModeProvider>
-        </UserProvider>
-      </NetworkStatusProvider>
+      <RouterApp />
     </BrowserRouter>
   </StrictMode>
 )
