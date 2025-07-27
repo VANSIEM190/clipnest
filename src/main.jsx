d4da13e169c78b8c5e731adbbc33101fe1b24140
VANSIEM190 <vansiemgracia03@gmail.com>
2025-07-27 04:03:40 +0100

    mise en place de cotext dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import RouterApp from './router/RouterApp'
-import { NetworkStatusProvider } from './context/NetworkStatusContext.jsx'
-import { UserProvider } from './context/UserContext.jsx'
-import { DarkModeProvider } from './context/DarkModeContext.jsx'
+import { DarkModeProvider , UserProvider , NetworkStatusProvider } from './context'
 
 
 createRoot(document.getElementById('root')).render(
